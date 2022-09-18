7e2f41358d86c739fd28fea5cbbc440eee3c3046
Anderson Neves <andersonneves_@hotmail.com>
2022-09-18 15:56:49 -0300

    creating function that add and remove arms

--- js/main.js
+++ js/main.js
@@ -1,29 +1,11 @@
-const robotron = document.querySelector("#robotron"); //criando uma variável
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-//robotron.addEventListener("click", dizOi); //o addEventListener é um escutador de eventos do tipo click, essa função sempre espera duas coisas, 1 um click, 2 o que ela irá fazer
+somar.addEventListener("click", (evento) => { 
+    braco.value = parseInt(braco.value) + 1;
+});
 
-//function dizOi() {  //função é uma forma de armazenar códigos que será executado várias vezes, ou seja, ala pode ser chamada mais de uma vez 
-//    console.log("oi!");
-//    console.log("Bem-vindo ao Robotron 2000");
-//}
-
-//dizOi(); //chamando a função, ou executando ela, uma função nós ajuda a repetir bloco de códigos
-
-//robotron.addEventListener("click", function() { //outra forma de criar uma função, também conhecida como "função anônima", ela só pode ser executada quando esse gatilho for desparado, ou seja, o evento do click, nunca em outro momento
-//    console.log("Cliquei no robô")
-//})
-
-//robotron.addEventListener("click", () => { //nas verções nova do javascript podemos escrever função anônima como arrow function, ou função de seta
-//    console.log("Cliquei no robô")
-//})
-
-function dizOi(nome) {  //função com parâmetro que recebe um nome
-    console.log("Oi " + nome); //imprime o nome
-    console.log("Bem-vindo ao Robotron 2000");
-}
-
-dizOi("Anderson"); //chamando a função e passando o nome
-
-robotron.addEventListener("click", (evento) => { //olha o evento na arrow function, ou função de seta, manipular o evento é muito importante
-    console.log(evento)
-})
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) - 1;
+});
