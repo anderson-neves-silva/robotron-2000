053f1444008a986a04c832353de70f9f01f3b32e
Anderson Neves <andersonneves_@hotmail.com>
2022-09-21 09:23:38 -0300

    improving powers code with date attributes, and making the code more dynamic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-
-const controle = document.querySelectorAll(".controle-ajuste"); //variável que guarda uma lista de todos os botões
+const controle = document.querySelectorAll("[data-controle]"); //agora ao invés de buscamos o a classe ".controle-ajuste", buscamos pelo "data attribute"
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
-        //console.log(evento.target.parentNode) //para ir no elemento que é o pai dele
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode); //o parentNode busca o elemento pai, aqui enviamos dois blocos de informação para a função manipula dados
     });
 });
 
-function manipulaDados(operacao, controle) { //função que reaproveita o código
-    const peca = controle.querySelector(".controle-contador");
+function manipulaDados(operacao, controle) { //função que controla dinamicamente e busca os elementos que estão sendo clicados acima, o controle é o elemento pai que está sendo clicado
+    const peca = controle.querySelector("[data-contador]"); //procurando a peça do data-contador apenas no controle
 
     if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1;    
