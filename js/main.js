8cdff07365469f207fa47b3b307218dde2ff711f
Anderson Neves <andersonneves_@hotmail.com>
2022-09-22 07:59:06 -0300

    function that updates statistics with parts data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const controle = document.querySelectorAll("[data-controle]"); //agora ao invés de buscamos o a classe ".controle-ajuste", buscamos pelo "data attribute"
-
-const pecas = {
+const controle = document.querySelectorAll("[data-controle]"); //buscamos todos os controles pelo "data attribute" para adicionar aquele evento.
+const estatisticas = document.querySelectorAll("[data-estatistica]"); //buscamos todos os elementos que tem as estatísticas. console.log(estatisticas);
+const pecas = { //aqui os valores a serem alterados a cada click.
     "bracos": {
         "forca": 29,
         "poder": 35,
@@ -35,16 +35,22 @@ const pecas = {
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode); //o parentNode busca o elemento pai, aqui enviamos dois blocos de informação para a função manipula dados
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode); //o parentNode busca o elemento pai, aqui enviamos dois blocos de informação para a função manipula dados.
+        atualizaEstatisticas(evento.target.dataset.peca); //aqui passa a peça.
     });
 });
 
-function manipulaDados(operacao, controle) { //função que controla dinamicamente e busca os elementos que estão sendo clicados acima, o controle é o elemento pai que está sendo clicado
-    const peca = controle.querySelector("[data-contador]"); //procurando a peça do data-contador apenas no controle
-
+function manipulaDados(operacao, controle) { //função que controla dinamicamente e busca os elementos que estão sendo clicados acima, o controle é o elemento pai que está sendo clicado.
+    const peca = controle.querySelector("[data-contador]"); //procurando a peça do data-contador apenas no controle.
     if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1;    
     } else {
         peca.value = parseInt(peca.value) + 1;    
     }
+}
+
+function atualizaEstatisticas(peca) { //função que atualiza as estatísticas com os dados das peças a cada click.
+    estatisticas.forEach( (elemento) => { //percorrendo todos os nomes em estatísticas quando clicado.
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]; //console.log(elemento.dataset.estatistica);
+    })
 }
